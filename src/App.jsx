42f8061ea30c91520cc1b3a704018012bd9555d5
lijiayin0420 <934432174@qq.com>
2020-05-08 18:58:09 +0800

    使用useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,44 @@
-import React, { Component, PureComponent, memo } from 'react'
-import './App.css'
+import React, { Component, useState } from 'react'
 
-/**
- * @class Foo
- * @extends {PureComponent}
- * 如果props的第一级发生变化才会重新渲染，深级变化不会渲染
- * 避免组件不必要的重新渲染，减少性能开销
- */
-// class Foo extends PureComponent {
-//   // shouldComponentUpdate(nextProps, nextState) {
-//   //   if (nextProps.name === this.props.name) {
-//   //     return false
-//   //   }
-//   //   return true
-//   // }
+// class App extends Component {
+//   state = {
+//     count: 0,
+//   }
 //   render() {
-//     console.log('Foo render')
-//   return <div>{this.props.person.age}</div>
+//     const { count } = this.state
+//     return (
+//       <div>
+//         <button
+//           type="button"
+//           onClick={() => {
+//             this.setState({ count: count + 1 })
+//           }}
+//         >
+//           Click ({count})
+//         </button>
+//       </div>
+//     )
 //   }
 // }
 
-/**
- * 无状态组件是函数式的，不能继承PureComponent
- * 但可使用memo包裹，达到同样的效果
- */
-const Foo = memo(function Foo(props) {
-  console.log('Foo render')
-  return <div>{props.person.age}</div>
-})
-
-class App extends Component {
-  state = {
-    count: 0,
-    person: {
-      age: 1,
-    },
-  }
-
-  // callback(){
-  //   //this指向无法保证
-  // }
-  callback = () => {}
-
-  render() {
-    const person = this.state.person
-    return (
-      <div>
-        <button
-          onClick={() => {
-            person.age++
-            this.setState({
-              count: this.state.count + 1,
-            })
-          }}
-        >
-          Add
-        </button>
-        <Foo person={person} name="Mike" cb={this.callback} />
-      </div>
-    )
-  }
+function App(props) {
+  // const defaultCount = props.defaultCount || 0
+  const [count, setCount] = useState(()=>{
+    //此逻辑只会执行一次
+    return props.defaultCount || 0
+  })
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Click ({count})
+      </button>
+    </div>
+  )
 }
 
 export default App
