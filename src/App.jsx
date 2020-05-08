aa6c1d68413013ea7c2732a693eb65b2c99b7c0c
lijiayin0420 <934432174@qq.com>
2020-05-08 16:49:10 +0800

    使用PureComponent，memo避免重新渲染

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,64 @@
-import React, { Component, lazy, Suspense } from 'react'
+import React, { Component, PureComponent, memo } from 'react'
 import './App.css'
 
-const About = lazy(() => import(/*webpackChunkName:'about'*/ './About.jsx'))
+/**
+ * @class Foo
+ * @extends {PureComponent}
+ * 如果props的第一级发生变化才会重新渲染，深级变化不会渲染
+ * 避免组件不必要的重新渲染，减少性能开销
+ */
+// class Foo extends PureComponent {
+//   // shouldComponentUpdate(nextProps, nextState) {
+//   //   if (nextProps.name === this.props.name) {
+//   //     return false
+//   //   }
+//   //   return true
+//   // }
+//   render() {
+//     console.log('Foo render')
+//   return <div>{this.props.person.age}</div>
+//   }
+// }
 
-//ErrorBoundary
-//compnentDidCatch
+/**
+ * 无状态组件是函数式的，不能继承PureComponent
+ * 但可使用memo包裹，达到同样的效果
+ */
+const Foo = memo(function Foo(props) {
+  console.log('Foo render')
+  return <div>{props.person.age}</div>
+})
 
 class App extends Component {
   state = {
-    hasError: false,
+    count: 0,
+    person: {
+      age: 1,
+    },
   }
 
-  static getDerivedStateFromError() {
-    return {
-      hasError: true,
-    }
-  }
-  // componentDidCatch() {
-  //   this.setState({
-  //     hasError: true,
-  //   })
+  // callback(){
+  //   //this指向无法保证
   // }
+  callback = () => {}
+
   render() {
-    if (this.state.hasError) {
-      return <div>error</div>
-    } else {
-      return (
-        <div>
-          <Suspense fallback={<div>loading</div>}>
-            <About></About>
-          </Suspense>
-        </div>
-      )
-    }
+    const person = this.state.person
+    return (
+      <div>
+        <button
+          onClick={() => {
+            person.age++
+            this.setState({
+              count: this.state.count + 1,
+            })
+          }}
+        >
+          Add
+        </button>
+        <Foo person={person} name="Mike" cb={this.callback} />
+      </div>
+    )
   }
 }
 
