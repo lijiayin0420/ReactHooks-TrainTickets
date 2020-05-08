41f7e7a5be0f72b82f0bb1957786bc4eacfc8a50
lijiayin0420 <934432174@qq.com>
2020-05-08 15:37:13 +0800

    练习使用createContext

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, { Component, createContext } from 'react'
+import './App.css'
+
+const BatteryContext = createContext(90) //90为默认值
+const OnlineContext = createContext()
+
+class Leaf extends Component {
+  render() {
+    return (
+      <BatteryContext.Consumer>
+        {(battery) => (
+          <OnlineContext.Consumer>
+            {(online) => (
+              <h1>
+                Battery:{battery},Online:{String(online)}
+              </h1>
+            )}
+          </OnlineContext.Consumer>
+        )}
+      </BatteryContext.Consumer>
+    )
+  }
+}
+
+class Middle extends Component {
+  render() {
+    return <Leaf />
+  }
+}
+
+class App extends Component {
+  state = {
+    battery: 60,
+    online: false,
+  }
+  render() {
+    const { battery, online } = this.state
+
+    return (
+      <BatteryContext.Provider value={battery}>
+        <OnlineContext.Provider value={online}>
+          <button
+            type="button"
+            onClick={() => this.setState({ battery: battery - 1 })}
+          >
+            Press
+          </button>
+          <button
+            type="button"
+            onClick={() => this.setState({ online: !online })}
+          >
+            Switch
+          </button>
+          <Middle />
+        </OnlineContext.Provider>
+      </BatteryContext.Provider>
+    )
+  }
+}
+
+export default App
