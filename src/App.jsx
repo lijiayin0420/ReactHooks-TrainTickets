580e40291f0f48f80fa350ae768e4fbc5b05c23f
lijiayin0420 <934432174@qq.com>
2020-05-08 19:23:21 +0800

    使用useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,35 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { useState, createContext, useContext, Component } from 'react'
 
-// class App extends Component {
-//   state = {
-//     count: 0,
-//   }
-//   render() {
-//     const { count } = this.state
-//     return (
-//       <div>
-//         <button
-//           type="button"
-//           onClick={() => {
-//             this.setState({ count: count + 1 })
-//           }}
-//         >
-//           Click ({count})
-//         </button>
-//       </div>
-//     )
-//   }
-// }
+const CountContext = createContext()
+
+class Foo extends Component {
+  render() {
+    return (
+      <CountContext.Consumer>
+        {(count) => <h1>{count}</h1>}
+      </CountContext.Consumer>
+    )
+  }
+}
+class Bar extends Component {
+  static contextType = CountContext
+  render() {
+    const count = this.context
+    return <h1>{count}</h1>
+  }
+}
+
+function Counter(){
+  const count = useContext(CountContext)
+  return (
+    <h1>{count}</h1>
+  )
+}
 
 function App(props) {
   // const defaultCount = props.defaultCount || 0
-  const [count, setCount] = useState(() => {
-    //此逻辑只会执行一次
-    return props.defaultCount || 0
-  })
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-  })
+  const [count, setCount] = useState(0)
 
-  const onResize = () => {
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-    })
-  }
-  useEffect(() => {
-    console.log(count)
-  }, [count])
-
-  useEffect(() => {
-    document.title = count
-  }, [])
-
-  useEffect(() => {
-    window.addEventListener('resize', onResize, false)
-    return () => {
-      window.removeEventListener('resize', onResize, false)
-    }
-  }, [])
-
-  const onClick = () => {
-    console.log('click')
-  }
-
-  useEffect(() => {
-    document.querySelector('#size').addEventListener('click', onClick)
-    return () => {
-      document.querySelector('#size').removeEventListener('click', onClick)
-    }
-  }, [])
   return (
     <div>
       <button
@@ -73,9 +40,11 @@ function App(props) {
       >
         Click ({count})
       </button>
-      <span id="size">
-        size:{size.width}x{size.height}
-      </span>
+      <CountContext.Provider value={count}>
+        <Foo />
+        <Bar />
+        <Counter />
+      </CountContext.Provider>
     </div>
   )
 }
