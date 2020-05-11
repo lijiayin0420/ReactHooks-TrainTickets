a18845c765628c86a9aca7fd95f4d34421bf75c8
lijiayin0420 <934432174@qq.com>
2020-05-11 17:37:24 +0800

    项目工程初始化，太不容易了！

---
+++ src/index/store.js
@@ -0,0 +1,16 @@
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux'
+
+import reducers from './reducers'
+import thunk from 'redux-thunk'
+
+export default createStore(
+    combineReducers(reducers),
+    {
+
+    },
+    applyMiddleware(thunk)
+)
---
+++ src/query/App.jsx
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux'
+import './App.css'
+
+function App(props){
+
+}
+
+export default connect(
+    function mapStateToProps(state){},
+    function mapDispatchToProps(dispatch){}
+)(App)
