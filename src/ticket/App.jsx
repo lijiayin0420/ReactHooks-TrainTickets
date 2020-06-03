32eeea95fbc4c9de4ba0bea4045443bd4c5b8ba2
lijiayin0420 <934432174@qq.com>
2020-06-03 15:18:03 +0800

    添加车票信息页相关

--- src/ticket/App.jsx
+++ src/ticket/App.jsx
@@ -1,11 +1,36 @@
+import React from 'react'
 import { connect } from 'react-redux'
+import Detail from '../common/Detail.jsx'
+import Candidate from './Candidate.jsx'
+import Schedule from './Schedule.jsx'
 import './App.css'
 
-function App(props){
+function App(props) {
+  const {
+    departDate,
+    arriveDate,
+    departTimeStr,
+    arriveTimeStr,
+    departStation,
+    arriveStation,
+    trainNumber,
+    durationStr,
+    tickets,
+    isScheduleVisible,
+    searchParsed,
+    dispatch,
+  } = props
 
+  return <div className='app'></div>
 }
 
 export default connect(
-    function mapStateToProps(state){},
-    function mapDispatchToProps(dispatch){}
+  function mapStateToProps(state) {
+    return state
+  },
+  function mapDispatchToProps(dispatch) {
+    return {
+      dispatch,
+    }
+  },
 )(App)
--- src/ticket/store.js
+++ src/ticket/store.js
@@ -10,7 +10,17 @@ import thunk from 'redux-thunk'
 export default createStore(
     combineReducers(reducers),
     {
-
+        departDate: Date.now(),
+        arriveDate: Date.now(),
+        departTimeStr: null,
+        arriveTimeStr: null,
+        departStation: null,
+        arriveStation: null,
+        trainNumber: null,
+        durationStr: null,
+        tickets: [],
+        isScheduleVisible: false,
+        searchParsed: false,
     },
     applyMiddleware(thunk)
 )
